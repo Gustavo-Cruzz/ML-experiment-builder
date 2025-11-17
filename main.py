568adb589e21d96a6efcbcb71f20543fbfa11ea2
Gustavo Cruz <82175584+Gustavo-Cruzz@users.noreply.github.com>
2025-11-17 16:55:56 -0300

    Standardized the routines train, test and eval

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import sys
 import yaml
 import mlflow
 import argparse
-from Workflow import train, test, evaluate
 from datetime import datetime
+from Workflow import train, test, evaluate
+from Datasets import TF_dataset_loader
 
 # Get the path to root directory
 project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -19,16 +20,17 @@ args = vars(parser.parse_args())
 timestamp = datetime.now().strftime("%m:%d:%y-%H-%M-%S")  # %H:%M:%S")
 
 
-def run_train(parameters, experiment_id):
-    train.train_routine(parameters, experiment_id)
+def run_train(parameters, experiment_id, dataset):
+    trainer = train.Train(parameters, experiment_id, dataset)
+    trainer.train_routine(dataset)
 
 
-def run_test():
-    test.test_routine(parameters, experiment_id)
+def run_test(parameters, experiment_id, dataset):
+    test.test_routine(parameters, experiment_id, dataset)
 
 
-def evaluate_model():
-    evaluate.eval_routine(parameters, experiment_id)
+def evaluate_model(parameters, experiment_id, dataset):
+    evaluate.eval_routine(parameters, experiment_id, dataset)
 
 
 if __name__ == "__main__":
@@ -36,15 +38,18 @@ if __name__ == "__main__":
         # Parse the YAML data
         parameters = yaml.safe_load(file)
 
+    
     experiment_id = mlflow.create_experiment(f"mlruns/{timestamp}")
     mlflow.start_run(experiment_id=experiment_id)
 
+    dataset = TF_dataset_loader.TensorFlowDataset(parameters["train"])
+    print(type(dataset))
     actions = {"train": run_train, "test": run_test, "eval": evaluate_model}
 
     for key in actions.keys():  # Runs the whole dict
         if parameters.get(key) is not None:  # Checks if input is in the yaml
             actions[key](
-                parameters[key], experiment_id
+                parameters[key], experiment_id, dataset
             )  # Sends filtered parameters for each step
 
     mlflow.end_run()
